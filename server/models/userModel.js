55e3393137e558fd583ba9c54516bf6c6395dbde
Aswathy <onenext329@gmail.com>
2026-03-03 15:50:47 +0530

    Updated user model with password hashing and compare method

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema(
   {
@@ -6,23 +7,27 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
       maxlength: 50,
+      trim: true,
     },
     email: {
       type: String,
       unique: true,
       required: true,
       lowercase: true,
+      trim: true,
       match: [/^\S+@\S+\.\S+$/, "Please enter a valid email"],
     },
     password: {
       type: String,
       required: true,
       minlength: 10,
+      select: false,
     },
     mobile: {
       type: String,
       required: true,
       unique: true,
+      match: [/^[0-9]{10}$/, "Enter valid 10 digit mobile number"],
     },
     role: {
       type: String,
@@ -32,5 +37,17 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
 export const User = mongoose.model("User", userSchema);
